354a80a9af538c8743331a75b3d75e7f40865cb0
Nestor Zepeda <NestorDZepeda@gmail.com>
2024-12-16 22:02:25 -0800

    Authentication works

---
+++ src/app/login/actions.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createServerClient } from "@/lib/auth/server";
+
+export async function login(formData: FormData) {
+  const supabase = await createServerClient();
+
+  // Todo: Validate inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createServerClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const result = await supabase.auth.signUp(data);
+
+  const { error } = result;
+
+  console.log({ data, result });
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}

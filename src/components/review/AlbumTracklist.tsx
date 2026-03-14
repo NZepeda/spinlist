06627892cb675cdbcc37c4b38bbd1fecfbc1379c
Nestor Zepeda <NestorDZepeda@gmail.com>
2026-03-14 19:14:16 -0400

    Fix build for real

--- src/components/review/AlbumTracklist.tsx
+++ src/components/review/AlbumTracklist.tsx
@@ -46,7 +46,7 @@ export function AlbumTracklist({
             variant="ghost"
             size="sm"
             onClick={() => {
-              onFavoriteTrackChange("");
+              onFavoriteTrackChange?.("");
             }}
           >
             Clear pick
